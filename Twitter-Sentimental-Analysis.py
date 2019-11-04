3c75fe58c711a445168d6332cdcca952c6bd1d3c
Jayesh Gharat <jayeshghrt7@gmail.com>
2019-11-04 07:36:36 +0530

    Update Twitter-Sentimental-Analysis.py
    bugs

--- Twitter-Sentimental-Analysis.py
+++ Twitter-Sentimental-Analysis.py
@@ -1,6 +1,7 @@
 import tweepy
 from textblob import TextBlob
 import os
+import csv
 
 # setting global variables
 
@@ -18,9 +19,15 @@ auth.set_access_token(access_token, access_token_secret)
 
 api = tweepy.API(auth)
 
+# Step 2 - File
+# Open/create a file to append data to
+csvFile = open('result.csv', 'a')
+
+#Use csv writer
+csvWriter = csv.writer(csvFile)
+
 #Step 3 - Retrieve Tweets
 public_tweets = api.search('Trump')
-
 
 
 #CHALLENGE - Instead of printing out each tweet, save each Tweet to a CSV file
@@ -36,15 +43,18 @@ for tweet in public_tweets:
     print(analysis.sentiment)
     print("")
 
+    csvWriter.writerow([tweet.created_at, analysis, analysis.sentiment])
+    print (tweet.created_at, analysis, analysis.sentiment)
 
- #Save the tweets in csv
-	#with open('%s_tweets.csv' % tweets, 'wb') as this_tweets_file:
-		#this_tweets_file.write('tweet,sentiment_label\n')
+csvFile.close()
 
-		    
+    #Save the tweets in csv
+    #with open('tweets.csv', 'wb') as this_tweets_file:
+         #this_tweets_file.write('analysis,analysis.sentiment\n')
+
             # recording the sentiment score
             #sentiment_score.insert(0, analysis.sentiment.polarity)
             # opening csv file to write the input along with tag and score
-            with open("tweet_sheet.csv", "a") as sheet:
-                W = csv.writer(sheet, delimiter=',')
-                W.writerow([tweet.text,analysis.sentiment])
+    #with open("tweet_sheet.csv", "a") as sheet:
+         #W = csv.writer(sheet, delimiter=',')
+         #W.writerow([tweet.text,analysis.sentiment])
